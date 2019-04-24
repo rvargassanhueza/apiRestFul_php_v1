e3d0e136e5f4e1f97f485445832ac0286faa1337
Rodrigo Vargas <innova3@racoop.loc>
2019-04-24 09:48:53 -0400

    comienzo a trabajar con otro validador

---
+++ src/helpers/validators/validator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace src\helpers\validators;
+
+use Respect\Validation\Validator as Respect;
+
+class Validator{
+    public function validate($request, array $rules){
+        
+    }
+}
